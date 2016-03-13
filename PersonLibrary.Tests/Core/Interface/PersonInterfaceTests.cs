aab16e4fc47d90b6074149b1dffca1e4fef0cbdf
Svetlozar Kirkov <svetlozark@gmail.com>
2016-03-14 01:07:17 +0200

    Unit tests structure, removed codeclimate (for now)

---
+++ PersonLibrary.Tests/Core/Interface/PersonInterfaceTests.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonLibrary.Tests.Core.Interface
+{
+    using Moq;
+    using NUnit.Framework;
+    using PersonLibrary.Core.Interface;
+
+    [TestFixture]
+    public class PersonInterfaceTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            // Arrange
+            var person = new Mock<IPerson>().Object;
+
+            // Act
+
+            // Assert
+        }
+    }
+}
