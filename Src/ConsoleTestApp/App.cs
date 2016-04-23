9ee2af55f34689b68f127a6e6be5a8355911adc6
Svetlozar Kirkov <svetlozark@gmail.com>
2016-04-23 10:39:21 +0300

    further restructuring

---
+++ Src/ConsoleTestApp/App.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleTestApp
+{
+    /// <summary>
+    /// Class App.
+    /// </summary>
+    /// TODO Edit XML Comment Template for App
+    internal class App
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// TODO Edit XML Comment Template for Main
+        private static void Main()
+        {
+        }
+    }
+}
